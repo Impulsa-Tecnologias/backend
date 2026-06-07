6d83c1409a21be9ef19bdf969b821da436abfac1
Cristian Burgos <cristian495225@gmail.com>
2026-06-07 16:15:35 -0500

    Ajustes para el guardado de recetas

--- src/main/java/com/edw/Cibot_Chat/repository/SavedRecipeRepository.java
+++ src/main/java/com/edw/Cibot_Chat/repository/SavedRecipeRepository.java
@@ -1,6 +1,7 @@
 package com.edw.Cibot_Chat.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +10,11 @@ import com.edw.Cibot_Chat.entity.SavedRecipe;
 public interface SavedRecipeRepository extends JpaRepository<SavedRecipe, Long>{
 
     List<SavedRecipe> findByUserIdOrderByCreatedAtDesc(Long userId);
+
+    boolean existsByUser_IdAndChat_IdAndRecipeTitleAndRecipeContent(Long userId, Long chatId, String title, String content);
+    boolean existsByUser_IdAndChat_IsNullAndRecipeTitleAndRecipeContent(Long userId, String title, String content);
+
+    Optional<SavedRecipe> findByUser_IdAndChat_IdAndRecipeTitleAndRecipeContent(Long userId, Long chatId, String title, String content);
+    Optional<SavedRecipe> findByUser_IdAndChat_IsNullAndRecipeTitleAndRecipeContent(Long userId, String title, String content);
     
 }
